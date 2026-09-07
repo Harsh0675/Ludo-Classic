d8ec32b9406897320487f8c18a6f5fa3f9ff2e52
HARSH NAGAR <157012443+Harsh0675@users.noreply.github.com>
2026-09-07 18:17:21 +0530

    Add main activity

---
+++ app/src/main/java/com/harshnagar/ludo/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.harshnagar.ludo
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.WindowManager
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        setContentView(LudoView(this))
+    }
+}
